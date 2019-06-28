7a2555c4f894bf758be89b7cce954a7689b208ca
Alex Lehman <j.alexander.lehman@gmail.com>
2019-06-28 13:51:15 -0400

    Add files via upload

--- src/blagario/AgarElements/World.cs
+++ src/blagario/AgarElements/World.cs
@@ -9,14 +9,14 @@ namespace blagario.elements
     {
         public List<AgarElement> Elements;
         public const long MaxMass = 60 * 1000;
-        public const long MaxViruses = 100;        
+        public const long MaxViruses = 100;
 
-        public override string CssStyle( Eyeglass c ) =>$@"
+        public override string CssStyle(Eyeglass c) => $@"
             top: {c.YGame2Physics(0)}px ;
             left: {c.XGame2Physics(0)}px;
             width: {X.ToString()}px ;
-            height: {X.ToString()}px ; 
-            "; 
+            height: {X.ToString()}px ;
+            ";
 
         public World(Universe universe)
         {
@@ -26,30 +26,30 @@ namespace blagario.elements
             Universe = universe;
         }
 
-        public long TotalMass => Elements.Sum( x=> x.Mass );
-        public IEnumerable<Virus> Viruses => Elements.Where(x => x.ElementType == ElementType.Virus ).Select(x=>x as Virus);
+        public IEnumerable<Virus> Viruses => Elements.Where(x => x.ElementType == ElementType.Virus).Select(x => x as Virus);
 
         public override async Task Tic()
         {
-            var currentElements = this.Elements.ToList();  // make a copy because TimerService is modifing the array.
+            var currentElements = Elements;  // make a copy because TimerService is modifing the array.
             foreach (var e in currentElements)
             {
                 await e.Tic();
             }
-            CheckIfWoldNedsMoreViruses();            
-            await Task.Delay(1);            
+            CheckIfWoldNedsMoreViruses();
+            await Task.Delay(1);
             await base.Tic();
         }
 
+        // Generates viruses on the board.
         private void CheckIfWoldNedsMoreViruses()
         {
-            while( Viruses.Count() < MaxViruses && TotalMass < MaxMass )
+            var currentElements = Elements.ToList();  // make a copy because TimerService is modifing the array.
+
+            while (Viruses.Count() < MaxViruses && currentElements.Sum(x => x.Mass) < MaxMass)
             {
                 // System.Console.WriteLine($"Creating new viruse. Current mass [{TotalMass}/{MaxMass}]. Current viruses [{Viruses.Count()}/{MaxViruses}]");
                 var newViruse = Virus.CreateVirus(this.Universe);
-                
             }
         }
-
     }
 }
